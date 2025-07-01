4e07257c39e2cd8ebff1994a263176e1ddd3426e
gitnassima20 <nassimabaroudi8@gmail.com>
2025-07-01 17:04:41 +0100

    Adding MaterialApp and Scaffold parts

--- lib/main.dart
+++ lib/main.dart
@@ -4,87 +4,31 @@ void main() {
   runApp(const MyApp());
 }
 
-String name = 'gitnassima20';
-int counterr = 0;
-double x = 1.0;
-List myList = ['S1', 'S2'];
-Map<String, dynamic> myMap = {'String': 'String', 'String2': 444};
+//stateless
+//material app
+//scaffold
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
+          seedColor: Colors.teal,
           brightness: Brightness.dark,
         ),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Map'),
+          leading: Icon(Icons.login),
+          actions: [Text('login'), Icon(Icons.login)],
+          backgroundColor: Colors.teal,
+        ),
       ),
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/images/git-icon.jfif',
-            fit: BoxFit.cover,
-            height: 300,
-          ),
-          SizedBox(height: 300, child: Center(child: Text('Flutter'))),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
