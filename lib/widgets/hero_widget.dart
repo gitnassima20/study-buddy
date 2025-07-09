fa3368d0a0e29dc8cb77e88aa56a8686377970c9
gitnassima20 <nassimabaroudi8@gmail.com>
2025-07-09 15:45:13 +0100

    redesign(hero): Enhance styling of HeroWidget

--- lib/widgets/hero_widget.dart
+++ lib/widgets/hero_widget.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 
 class HeroWidget extends StatelessWidget {
-  const HeroWidget({super.key});
+  const HeroWidget({super.key, required this.title});
 
+  final String title;
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: 'myhero',
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20.0),
-        child: Image.asset(
-          'assets/images/git-icon.jfif',
-          color: Colors.teal,
-          colorBlendMode: BlendMode.modulate,
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Hero(
+          tag: 'myhero',
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: SizedBox(
+              height: 200.0,
+              width: double.infinity,
+              child: Image.asset(
+                'assets/images/nature.png',
+                fit: BoxFit.cover,
+                color: Colors.teal,
+                colorBlendMode: BlendMode.modulate,
+              ),
+            ),
+          ),
         ),
-      ),
+        FittedBox(
+          child: Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+              letterSpacing: 8.0,
+              color: Colors.white60,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
