90444f23726a7b86a1a85e59b50279e0474141a0
gitnassima20 <nassimabaroudi8@gmail.com>
2025-07-10 12:32:30 +0100

    feat(design): Added constant foor app colors

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -13,3 +13,12 @@ class KTextStyle {
     color: Colors.teal,
   );
 }
+
+class AppColors {
+  static const Color tealGreen = Color(0xFF2C8C82);
+  static const Color navyBlue = Color(0xFF3E5A9A);
+  static const Color warmPeach = Color(0xFFF8CDA0);
+  static const Color sageGreen = Color(0xFF5FA38B);
+  static const Color skyBlue = Color(0xFFA8C8E5);
+  static const Color cloudWhite = Color(0xFFF0F2F5);
+}
